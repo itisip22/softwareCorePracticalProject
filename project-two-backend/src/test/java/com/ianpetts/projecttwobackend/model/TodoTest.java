601f14c21e302027d30485cfa0ffed64ab0f43da
Ian Petts <itisianpetts@gmail.com>
2022-08-14 12:09:33 +0100

    Update TodoTest.java

--- project-two-backend/src/test/java/com/ianpetts/projecttwobackend/model/TodoTest.java
+++ project-two-backend/src/test/java/com/ianpetts/projecttwobackend/model/TodoTest.java
@@ -1,46 +1,36 @@
 package com.ianpetts.projecttwobackend.model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TodoTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    void testConstructor() {
+        Todo actualTodo = new Todo();
+        actualTodo.setDescription("Constructor Test 1");
+        actualTodo.setId(1);
+        actualTodo.setTitle("Test 1");
+        String actualToStringResult = actualTodo.toString();
+        assertEquals("Constructor Test 1", actualTodo.getDescription());
+        assertEquals(1, actualTodo.getId().intValue());
+        assertEquals("Test 1", actualTodo.getTitle());
+        assertEquals("Todo{id=1, title='Test 1', description='Constructor Test 1'}",
+                actualToStringResult);
     }
 
     @Test
-    void getId() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void getTitle() {
-    }
-
-    @Test
-    void setTitle() {
-    }
-
-    @Test
-    void getDescription() {
-    }
-
-    @Test
-    void setDescription() {
-    }
-
-    @Test
-    void testToString() {
+    void testConstructor2() {
+        Todo actualTodo = new Todo(1, "Test 2", "Constructor Test 2");
+        actualTodo.setDescription("Constructor Test 2.1");
+        actualTodo.setId(1);
+        actualTodo.setTitle("Test 2.1");
+        String actualToStringResult = actualTodo.toString();
+        assertEquals("Constructor Test 2.1", actualTodo.getDescription());
+        assertEquals(1, actualTodo.getId().intValue());
+        assertEquals("Test 2.1", actualTodo.getTitle());
+        assertEquals("Todo{id=1, title='Test 2.1', description='Constructor Test 2.1'}",
+                actualToStringResult);
     }
 }
+
